9fc2381f279cdfd580d71d62e39b4bb0cfbd6c94
Marko Jovanovic <marko07031997@gmail.com>
2020-01-14 13:23:48 +0100

    Creating main.py file

---
+++ main.py
@@ -0,0 +1,11 @@
+#from ime_filea import ime_funkcije
+
+
+def main():
+    print('RUN')
+    txt = input("Type something: ")
+    print("You wrote: " + txt)
+
+
+if __name__ == "__main__":
+    main()
